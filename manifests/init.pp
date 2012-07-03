72785c318738c340d98eddb554b5d1317de7a00c
Sebastien Badia <seb@sebian.fr>
2012-07-03 20:57:47 +0200

    add params, and user setup

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,34 @@
-# Module:: gitlab
-# Manifest:: init.pp
+# == Class: gitlab
 #
-# Author:: Sebastien Badia (<seb@sebian.fr>)
-# Date:: Tue Jul 03 20:06:33 +0200 2012
-# Maintainer:: Sebastien Badia (<seb@sebian.fr>)
+# === Parameters
 #
+# [git_home]
+# [git_comment]
+# [gitlab_home]
+# [gitlab_comment]
+#
+# === Examples
+#
+# === Authors
+#
+# Sebastien Badia (<seb@sebian.fr>)
+#
+# === Copyright
+#
+# Sebastien Badia © 2012
+# Tue Jul 03 20:06:33 +0200 2012
 
 # Class:: gitlab
 #
 #
-class gitlab {
+class gitlab(
+  $git_user       = 'git',
+  $git_home       = '/home/git',
+  $git_comment    = 'git version control',
+  $git_adminkey   = '',
+  $gitlab_user    = 'gitlab',
+  $gitlab_home    = '/home/gitlab',
+  $gitlab_comment = 'gitlab system') {
   case $operatingsystem {
     debian,ubuntu: {
       include "gitlab::base"
@@ -32,5 +51,25 @@ class gitlab::base {
      "redis-server","openssh-server","python-dev","python-pip","libyaml-dev",
      "ruby1.9.1","ruby1.9.1-dev"]:
       ensure => installed;
+  }
+
+  user {
+    $git_user:
+      ensure  => present,
+      shell   => '/bin/sh',
+      home    => $git_home, managehome => true,
+      comment => $git_comment, system => true;
+    $gitlab_user:
+      ensure  => present,
+      groups  => 'git', shell => '/bin/bash',
+      home    => $gitlab_home, managehome => true,
+      comment => $gitlab_comment;
+  }
+
+  file {
+    "/var/cache/debconf/gitolite.preseed":
+      content => template('gitlab/gitolite.preseed.erb'),
+      ensure  => file,
+      before  => Package["gitolite"];
   }
 } # Class:: gitlab::base
