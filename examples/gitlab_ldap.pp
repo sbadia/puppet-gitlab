e8409360c2a9c7e9849fd78e801e860e9d66dc5e
Sebastien Badia <seb@sebian.fr>
2012-10-27 19:11:09 +0200

    Add ldap example

---
+++ examples/gitlab_ldap.pp
@@ -0,0 +1,28 @@
+# Configure a gitlab server with LDAP auth (gitlab.domain.tld)
+node /gitlab_server/ {
+  class {
+    'gitlab':
+      git_user          => 'git',
+      git_home          => '/home/git',
+      git_email         => 'notifs@foobar.fr',
+      git_comment       => 'GIT control version',
+      # Default provider for ssh keys is 'source'
+      # you can use also   => 'puppet:///modules/gitlab/file'
+      # fileserving on http doesn't work yet (http://projects.puppetlabs.com/issues/5783)
+      # If you choose ssh_key_provider = 'content'
+      # you can use directly => 'ssh-rsa AAA...'
+      git_admin_pubkey  => '/srv/vagrant-puppet/manifests/gitlab_testing.pub',
+      git_admin_privkey => '/srv/vagrant-puppet/manifests/gitlab_testing.priv',
+      gitlab_user       => 'gitlab',
+      gitlab_home       => '/home/gitlab',
+      gitlab_comment    => 'GITLab is awesome',
+      #FIXME mysql db not yet created, see https://github.com/sbadia/puppet-gitlab/issues/11
+      gitlab_dbtype     => 'mysql',
+      ldap_enable       => true,
+      ldap_host         => 'ldap.foobar.fr',
+      ldap_base         => 'dc=foobar,dc=fr',
+      ldap_uid          => 'uid',
+      ldap_port         => '636',
+      ldap_method       => 'ssl',
+  }
+}
