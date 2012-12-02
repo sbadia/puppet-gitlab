f703bc0aca8441636caa3d0bd8c4736906ced770
Sebastien Badia <seb@sebian.fr>
2012-12-02 11:26:56 +0100

    [pre] revert for apt update (already needed on my debian img)

--- manifests/pre.pp
+++ manifests/pre.pp
@@ -81,8 +81,7 @@ class gitlab::debian_packages {
 
   exec {
     'apt-get update':
-      command     => '/usr/bin/apt-get update',
-      refreshonly => true;
+      command     => '/usr/bin/apt-get update';
   }
 
   package {
