b79023a7d0d67ce58c9c8cbc4e5d51d1ea412bcd
Sebastien Badia <seb@sebian.fr>
2014-02-22 02:02:11 +0100

    [spec] Add spec tests for #116 (relative_url_root)

--- spec/classes/gitlab_install_spec.rb
+++ spec/classes/gitlab_install_spec.rb
@@ -12,35 +12,36 @@ describe 'gitlab' do
   # a non-default common parameter set
   let :params_set do
     {
-      :git_user               => 'gitlab',
-      :git_home               => '/srv/gitlab',
-      :git_comment            => 'Labfooboozoo',
-      :git_email              => 'gitlab@fooboozoo.fr',
-      :gitlab_sources         => 'https://github.com/gitlabhq/gitlabhq',
-      :gitlab_branch          => '4-2-stable',
-      :gitlabshell_sources    => 'https://github.com/gitlabhq/gitlab-shell',
-      :gitlabshell_branch     => 'v1.2.3',
-      :gitlab_repodir         => '/mnt/nas',
-      :gitlab_redishost       => 'redis.fooboozoo.fr',
-      :gitlab_redisport       => '9736',
-      :gitlab_dbname          => 'gitlab_production',
-      :gitlab_dbuser          => 'baltig',
-      :gitlab_dbpwd           => 'Cie7cheewei<ngi3',
-      :gitlab_dbhost          => 'sql.fooboozoo.fr',
-      :gitlab_dbport          => '2345',
-      :gitlab_http_timeout    => '300',
-      :gitlab_projects        => '42',
-      :gitlab_username_change => false,
-      :gitlab_unicorn_port    => '8888',
-      :gitlab_unicorn_worker  => '8',
-      :exec_path              => '/opt/bw/bin:/bin:/usr/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/sbin',
-      :ldap_host              => 'ldap.fooboozoo.fr',
-      :ldap_base              => 'dc=fooboozoo,dc=fr',
-      :ldap_port              => '666',
-      :ldap_uid               => 'cn',
-      :ldap_method            => 'tls',
-      :ldap_bind_dn           => 'uid=gitlab,o=bots,dc=fooboozoo,dc=fr',
-      :ldap_bind_password     => 'aV!oo1ier5ahch;a'
+      :git_user                 => 'gitlab',
+      :git_home                 => '/srv/gitlab',
+      :git_comment              => 'Labfooboozoo',
+      :git_email                => 'gitlab@fooboozoo.fr',
+      :gitlab_sources           => 'https://github.com/gitlabhq/gitlabhq',
+      :gitlab_branch            => '4-2-stable',
+      :gitlabshell_sources      => 'https://github.com/gitlabhq/gitlab-shell',
+      :gitlabshell_branch       => 'v1.2.3',
+      :gitlab_repodir           => '/mnt/nas',
+      :gitlab_redishost         => 'redis.fooboozoo.fr',
+      :gitlab_redisport         => '9736',
+      :gitlab_dbname            => 'gitlab_production',
+      :gitlab_dbuser            => 'baltig',
+      :gitlab_dbpwd             => 'Cie7cheewei<ngi3',
+      :gitlab_dbhost            => 'sql.fooboozoo.fr',
+      :gitlab_dbport            => '2345',
+      :gitlab_relative_url_root => '/myfoobooforge',
+      :gitlab_http_timeout      => '300',
+      :gitlab_projects          => '42',
+      :gitlab_username_change   => false,
+      :gitlab_unicorn_port      => '8888',
+      :gitlab_unicorn_worker    => '8',
+      :exec_path                => '/opt/bw/bin:/bin:/usr/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/sbin',
+      :ldap_host                => 'ldap.fooboozoo.fr',
+      :ldap_base                => 'dc=fooboozoo,dc=fr',
+      :ldap_port                => '666',
+      :ldap_uid                 => 'cn',
+      :ldap_method              => 'tls',
+      :ldap_bind_dn             => 'uid=gitlab,o=bots,dc=fooboozoo,dc=fr',
+      :ldap_bind_password       => 'aV!oo1ier5ahch;a'
     }
   end
 
@@ -122,6 +123,7 @@ describe 'gitlab' do
         end
         it { should contain_file('/home/git/gitlab/config/gitlab.yml').with_content(/port: 80/)}
         it { should contain_file('/home/git/gitlab/config/gitlab.yml').with_content(/https: false/)}
+        it { should contain_file('/home/git/gitlab/config/gitlab.yml').with_content(/relative_url_root: \//)}
         it { should contain_file('/home/git/gitlab/config/gitlab.yml').with_content(/email_from: git@someserver.net/)}
         it { should contain_file('/home/git/gitlab/config/gitlab.yml').with_content(/default_projects_limit: 10/)}
         it { should contain_file('/home/git/gitlab/config/gitlab.yml').with_content(/username_changing_enabled: true/)}
@@ -274,6 +276,7 @@ describe 'gitlab' do
             it { should contain_file("#{params_set[:git_home]}/gitlab/config/gitlab.yml").with_content(/https: true/)}
           end
         end
+        it { should contain_file("#{params_set[:git_home]}/gitlab/config/gitlab.yml").with_content(/relative_url_root: #{params_set[:gitlab_relative_url_root]}/)}
         it { should contain_file("#{params_set[:git_home]}/gitlab/config/gitlab.yml").with_content(/email_from: #{params_set[:git_email]}/)}
         it { should contain_file("#{params_set[:git_home]}/gitlab/config/gitlab.yml").with_content(/default_projects_limit: #{params_set[:gitlab_projects]}/)}
         it { should contain_file("#{params_set[:git_home]}/gitlab/config/gitlab.yml").with_content(/username_changing_enabled: #{params_set[:gitlab_username_change]}/)}
