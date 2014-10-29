3696590719a66e9cf115f69a4ec9ed5d9e809781
Emanuele 'Tex' Tessore <setola@gmail.com>
2014-10-29 14:12:21 +0100

    fixed future parser casting issues

--- manifests/init.pp
+++ manifests/init.pp
@@ -359,8 +359,8 @@ class gitlab(
     $use_exim                 = $gitlab::params::use_exim,
   ) inherits gitlab::params {
   case $::osfamily {
-    Debian: {}
-    Redhat: {
+    'Debian': {}
+    'Redhat': {
       warning("${::osfamily} not fully tested with ${gitlab_branch}")
     }
     default: {
