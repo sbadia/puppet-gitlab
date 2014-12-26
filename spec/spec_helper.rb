cd24c31c9c7a65bbf67feb3c15efad19743e4bbb
Sebastien Badia <seb@sebian.fr>
2014-12-26 22:39:33 +0100

    unmanage spec/spec_helper.rb and revert changes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,21 @@
-# Managed by modulesync
-# Configs https://github.com/sbadia/modulesync_configs/
-#
 require 'puppetlabs_spec_helper/module_spec_helper'
+require 'shared_examples'
+
+RSpec.configure do |c|
+  c.alias_it_should_behave_like_to :it_configures, 'configures'
+  c.alias_it_should_behave_like_to :it_raises, 'raises'
+
+  c.default_facts = {
+    :osfamily               => 'Debian',
+    :operatingsystem        => 'Debian',
+    :kernel                 => 'Linux',
+    :lsbdistid              => 'debian',
+    :lsbdistcodename        => 'wheezy',
+    :operatingsystemrelease => '6.5',
+    :fqdn                   => 'gitlab.fooboozoo.fr',
+    :processorcount         => '2',
+    :sshrsakey              => 'AAAAB3NzaC1yc2EAAAA',
+    :concat_basedir         => '/var/lib/puppet/concat'
+  }
+
+end
